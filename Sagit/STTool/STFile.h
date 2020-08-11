73910aacbd27f64539747ee051cdaed0f4e9e914
Aster <272657997@qq.com>
2020-08-11 20:21:48 +0800

    增加说明

--- Sagit/STTool/STFile.h
+++ Sagit/STTool/STFile.h
@@ -8,20 +8,20 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-//!用于存档数据到plist文件中,默认存档沙盒的Library/Cache目录
+//!用于存档数据到plist文件中,默认存档沙盒的Library/Cache目录（iTunes不会备份此目录，此目录下文件不会在应用退出删除。一般存放体积比较大，不是特别重要的资源，比如缓存数据。缓存数据在设备低存储空间时可能会被删除。）
 @interface STFile : NSObject
 
-//!对应沙盒的Home目录
+//!对应沙盒的Home目录（主目录）
 @property (nonatomic,retain) STFile* Home;
-//!对应沙盒的Document目录
+//!对应沙盒的Document目录：用于存储用户数据，该目录下的所有文件会进行iCloud或iTunes备份
 @property (nonatomic,retain) STFile* Document;
-//!对应沙盒的Libaray目录
+//!对应沙盒的Libaray目录（该路径下的文件夹，除Caches以外，都会被iTunes备份。）
 @property (nonatomic,retain) STFile* Libaray;
-//!对应沙盒的Tmp目录
+//!对应沙盒的Tmp目录（目录用于存放临时文件，APP重新启动时会清除这个路径下的文件。该路径下的文件不会被iTunes备份。一般用来保存临时文件，比如:相机拍摄完成时的照片视频都会被暂时保存到这个路径。）
 @property (nonatomic,retain) STFile* Temp;
-//!存档系统配置信息，对应沙盒的Tmp目录 Library/Preferences
+//!存档系统配置信息，对应沙盒的Tmp目录 Library/Preferences（包含应用程序的偏好设置文件。NSUserDefaults就是默认存放在此文件夹下面。）
 @property (nonatomic,retain)NSUserDefaults* Setting;
-//!存档的文件名。
+//!存档的文件名（plist）。
 @property (readonly,nonatomic,copy) NSString* fileName;
 
 
