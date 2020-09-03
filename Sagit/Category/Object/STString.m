8961df4b01fdd6597c7fe52ddf0b24f31ccd9d27
Aster <272657997@qq.com>
2020-09-03 16:33:38 +0800

    修正isNilOrEmpty判断。

--- Sagit/Category/Object/STString.m
+++ Sagit/Category/Object/STString.m
@@ -87,7 +87,7 @@
 -(BOOL)contains:(NSString *)value{return [self containsString:value];}
 -(BOOL)contains:(NSString *)value ignoreCase:(BOOL)ignoreCase{return [self indexOf:value ignoreCase:ignoreCase]>=0;}
 -(BOOL)isEmpty{return [self isEqualToString:@""];}
-+(BOOL)isNilOrEmpty:(NSString*)value{return value==nil || [value isEmpty] || [value isKindOfClass:[NSNull class]];}
++(BOOL)isNilOrEmpty:(NSString*)value{return value==nil || [value isKindOfClass:[NSNull class]] || [value isEmpty];}
 -(BOOL)eq:(id)value
 {
     if([value isKindOfClass:[NSNumber class]])
