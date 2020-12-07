84655b4b0fee61100dc4de4c5d38b0c02da99837
Aster <272657997@qq.com>
2020-12-07 15:27:16 +0800

    更新定义。

--- Sagit/Define/STDefine.h
+++ Sagit/Define/STDefine.h
@@ -10,26 +10,26 @@
 @interface STDefine : NSObject
 + (instancetype)share;
 #pragma mark UI编码规范【重要】
-//【可更改】选择编码标准：1倍（375*667）、2倍（750*1334）【默认值】、3倍（1125*2001）
+///【可更改】选择编码标准：1倍（375*667）、2倍（750*1334）【默认值】、3倍（1125*2001）
 @property (nonatomic,assign) NSInteger StandardScale;
 
 #pragma mark 状态栏、导航栏、Tab栏 默认显示定义 【可选】
-// 状态栏 的默认显示 YES
+/// 状态栏 的默认显示 YES
 @property (nonatomic,assign) BOOL DefaultShowStatus;
-// 有NavController 的默认显示 YES
+/// 有NavController 的默认显示 YES
 @property (nonatomic,assign) BOOL DefaultShowNav;
-// 有TabController 的默认显示 YES
+/// 有TabController 的默认显示 YES
 @property (nonatomic,assign) BOOL DefaultShowTab;
 
 #pragma mark 导航栏 【可选】
-///可设定名称 【默认返回文字】
+/// 可设定名称 【默认返回文字】
 @property (nonatomic,copy) NSString *DefaultNavLeftTitle;
-///可设定图标 【默认返回图标】
+/// 可设定图标 【默认返回图标】
 @property (nonatomic,copy) NSString *DefaultNavLeftImage;
 #pragma mark 手机屏幕默认的显示方向（默认：竖立） 【可选】
-//可设定手机屏幕默认的显示方向
+/// 可设定手机屏幕默认的显示方向
 @property (nonatomic,assign) UIInterfaceOrientation DefaultOrientation;
-////可设定手机屏幕默认支持的显示方向
+/// 可设定手机屏幕默认支持的显示方向
 @property (nonatomic,assign) UIInterfaceOrientationMask DefaultOrientationMask;
 @end
 
--- Sagit/Define/STDefine.m
+++ Sagit/Define/STDefine.m
@@ -5,6 +5,15 @@
 
 #import "STDefine.h"
 @interface STDefine()
+@property (nonatomic,assign) NSInteger _StandardScale;
+@property (nonatomic,assign) NSInteger _DefaultShowNav;
+@property (nonatomic,assign) NSInteger _DefaultShowTab;
+@property (nonatomic,assign) NSInteger _DefaultShowStatus;
+@property (nonatomic,copy) NSString *_DefaultNavLeftTitle;
+@property (nonatomic,copy) NSString *_DefaultNavLeftImage;
+@property (nonatomic,assign) NSUInteger _DefaultOrientation;
+@property (nonatomic,assign) NSUInteger _DefaultOrientationMask;
+
 @end
 @implementation STDefine
 
@@ -20,30 +29,99 @@
 }
 - (NSInteger)StandardScale
 {
+    if(self._StandardScale>0 && self._StandardScale<4)
+    {
+        return self._StandardScale;
+    }
     return 2;
+}
+-(void)setStandardScale:(NSInteger)StandardScale
+{
+    self._StandardScale=StandardScale;
 }
 - (BOOL)DefaultShowNav
 {
+    if(self._DefaultShowNav>0)
+    {
+        return self._DefaultShowNav==1;
+    }
     return YES;
 }
+- (void)setDefaultShowNav:(BOOL)DefaultShowNav
+{
+    self._DefaultShowNav=DefaultShowNav?1:2;
+}
+
 - (BOOL)DefaultShowTab
 {
+    if(self._DefaultShowTab>0)
+    {
+        return self._DefaultShowTab==1;
+    }
     return YES;
+}
+- (void)setDefaultShowTab:(BOOL)DefaultShowTab
+{
+    self._DefaultShowTab=DefaultShowTab?1:2;
+}
+-(BOOL)DefaultShowStatus
+{
+    if(self._DefaultShowStatus>0)
+    {
+        return self._DefaultShowStatus==1;
+    }
+    return YES;
+}
+- (void)setDefaultShowStatus:(BOOL)DefaultShowStatus
+{
+    self._DefaultShowStatus=DefaultShowStatus?1:2;
 }
 - (NSString *)DefaultNavLeftTitle
 {
+    if(self._DefaultNavLeftTitle!=nil)
+    {
+        return self._DefaultNavLeftTitle;
+    }
     return @"STEmpty";
+}
+-(void)setDefaultNavLeftTitle:(NSString *)DefaultNavLeftTitle
+{
+    self._DefaultNavLeftTitle=DefaultNavLeftTitle;
 }
 -(NSString *)DefaultNavLeftImage
 {
-    return nil;
+    return self._DefaultNavLeftImage;
 }
+-(void)setDefaultNavLeftImage:(NSString *)DefaultNavLeftImage
+{
+    self._DefaultNavLeftImage=DefaultNavLeftImage;
+}
+
+
 - (UIInterfaceOrientation)DefaultOrientation
 {
+    if(self._DefaultOrientation!= 0)
+    {
+        return (UIInterfaceOrientation)self._DefaultOrientation;
+    }
     return UIInterfaceOrientationPortrait;
 }
+-(void)setDefaultOrientation:(UIInterfaceOrientation)DefaultOrientation
+{
+    self._DefaultOrientation=(NSInteger)DefaultOrientation;
+}
+
+
 -(UIInterfaceOrientationMask)DefaultOrientationMask
 {
+    if(self._DefaultOrientationMask!= 0)
+    {
+        return (UIInterfaceOrientationMask)self._DefaultOrientationMask;
+    }
     return UIInterfaceOrientationMaskAllButUpsideDown;
+}
+-(void)setDefaultOrientationMask:(UIInterfaceOrientationMask)DefaultOrientationMask
+{
+    self._DefaultOrientationMask=(NSInteger)DefaultOrientationMask;
 }
 @end
